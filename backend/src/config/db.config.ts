6a52323e43a8096b77388b9e147cbaea2d49c171
M-Fayiz <fayizmuhammedmuhammedfayiz@gmail.com>
2026-02-10 14:56:57 +0530

    feat: connect database to server

---
+++ backend/src/config/db.config.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import envConfig from "./env.config"; 
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(envConfig.MONGO_URL as string);
+
+    console.log("DB CONNECTED |");
+  } catch (error) {
+    console.log("DB Error : ", error);
+  }
+};
